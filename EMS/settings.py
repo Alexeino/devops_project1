372e55f9b67c1cd266d3c956b5b288f7b8610a12
Manish Sharma <ms8750122327@gmail.com>
2022-10-16 19:34:54 +0530

    Ready to be deployed over VM

--- EMS/settings.py
+++ EMS/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'employees_db',
-        'USER': 'root',
-        'PASSWORD': 'M#ni7h@2024',
-        'HOST': '127.0.0.1',  
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': '192.168.10.12',  
         'PORT': '3306',  
         'OPTIONS': {  
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
