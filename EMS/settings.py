041975d0196af8f10462fed5353ffd616060b999
Manish Sharma <ms8750122327@gmail.com>
2022-10-17 14:43:09 +0530

    downgraded django for mysqlclient

--- EMS/settings.py
+++ EMS/settings.py
@@ -77,6 +77,7 @@ WSGI_APPLICATION = 'EMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.mysql',
